c6a8091b3fa42f01b9ebfeee271d90748a0b1c3f
upalr <shovon.forums@gmail.com>
2018-04-28 23:43:37 +1000

    first commit

---
+++ Project 1/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Account
+    {
+        private static uint Id;
+        [Required]
+        public DateTime openedDate { get; }
+        public DateTime closedDate { get; }
+        public bool active { get; }
+        public double balance { get; set; }
+        public Customer owner { get; }
+
+        public Account(Customer owner, DateTime openedDate, double balance)
+        {
+            this.owner = owner;
+            this.openedDate = openedDate;
+            this.balance = balance;
+            active = true;
+        }
+
+        public Account(Customer owner, double balance) : this(owner, DateTime.Now, balance)
+        {
+
+        }
+
+        public void Close()
+        {
+            if(active == true)
+            {
+                //active = false;
+                //closedDate = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ Project 1/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_1
+{
+    class Customer
+    {
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public DateTime dateOfBirth { get; set; }
+        public int contactNumber { get; set; }
+        public string email { get; set; }
+
+        private List<Account> accountList = new List<Account>();
+
+        public Customer(string firstName, string lastName, string address, DateTime dateOfBirth, int contactNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.dateOfBirth = dateOfBirth;
+            this.contactNumber = contactNumber;
+            this.email = email;
+        }
+
+        public override string ToString()
+        {
+            return "First Name: " + firstName +
+                "Last Name: " + lastName + 
+                "Address: " + address +
+                "Date of birth: " + dateOfBirth + 
+                "Contact Number: " + contactNumber + 
+                "Email: " + email +
+                "Total Balance: " + SumBalance();
+        }
+
+        private string SumBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccounts()
+        {
+            return accountList;
+        }
+    }
+}
